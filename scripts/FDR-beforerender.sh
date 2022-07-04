4b5c96caacf18da7b5fa9e2bb691ca8e12f08b2a
fbeauKmi <88246672+fbeauKmi@users.noreply.github.com>
2022-07-04 02:08:11 +0200

    Delete FDR-beforerender.sh

--- scripts/FDR-beforerender.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-SONY_CAM="http://192.168.122.1:10000/sony/camera";
-HEADER="application/json, charset=utf-8";
-
-SHOOTMODE=$(curl -s $SONY_CAM -X POST -H "${HEADER}" --max-time 2 -d '{"id":1,"method":"setShootMode","params":["movie"],"version":"1.0"}')>/dev/null 
-PICTURE=$(curl -s $SONY_CAM -X POST -H "${HEADER}" --max-time 1 -d '{"id":2, "method":"startMovieRec","params":[],"version":"1.0"}')
-
